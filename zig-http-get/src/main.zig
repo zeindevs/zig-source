c8dae828c81b2c377a6f00f4ac788a3d55d410c7
zeindevs <zeindevs@gmail.com>
2024-07-12 11:15:28 +0700

    add: zig-http-get

---
+++ zig-http-get/src/main.zig
@@ -0,0 +1,37 @@
+const std = @import("std");
+const print = std.debug.print;
+const http = std.http;
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    var client = http.Client{ .allocator = allocator };
+    defer client.deinit();
+
+    const uri = try std.Uri.parse("https://httpbin.org/headers");
+    const buf = try allocator.alloc(u8, 1024 * 1024 * 4);
+    defer allocator.free(buf);
+    var req = try client.open(.GET, uri, .{
+        .server_header_buffer = buf,
+    });
+    defer req.deinit();
+
+    try req.send();
+    try req.finish();
+    try req.wait();
+
+    var iter = req.response.iterateHeaders();
+    while (iter.next()) |header| {
+        std.debug.print("Name:{s}, Value:{s}\n", .{ header.name, header.value });
+    }
+
+    try std.testing.expectEqual(req.response.status, .ok);
+
+    var rdr = req.reader();
+    const body = try rdr.readAllAlloc(allocator, 1024 * 1024 * 4);
+    defer allocator.free(body);
+
+    print("Body:\n{s}\n", .{body});
+}
