0f0202b3a3103838b3e58ad10fa361097cb75fbc
zeindevs <zeindevs@gmail.com>
2024-07-12 11:16:11 +0700

    add: zig-http-post

---
+++ zig-http-post/src/main.zig
@@ -0,0 +1,40 @@
+const std = @import("std");
+const print = std.debug.print;
+const http = std.http;
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    var client = http.Client{ .allocator = allocator };
+    defer client.deinit();
+
+    const uri = try std.Uri.parse("https://httpbin.org/anything");
+
+    const payload = 
+        \\ {
+        \\  "name": "zig-cookbook",
+        \\  "author": "John"
+        \\ }
+    ;
+
+    var buf: [1204]u8 = undefined;
+    var req = try client.open(.POST, uri, .{ .server_header_buffer = &buf });
+    defer req.deinit();
+
+    req.transfer_encoding = .{ .content_length = payload.len };
+    try req.send();
+    var wtr = req.writer();
+    try wtr.writeAll(payload);
+    try req.finish();
+    try req.wait();
+
+    try std.testing.expectEqual(req.response.status, .ok);
+
+    var rdr = req.reader();
+    const body = try rdr.readAllAlloc(allocator, 1204 * 1204 * 4);
+    defer allocator.free(body);
+
+    print("Body:\n{s}\n", .{body});
+}
