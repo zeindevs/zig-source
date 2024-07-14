268fc0e0cc4ce07bf6ed9f4070a78b856bfa1b1f
zeindevs <zeindevs@gmail.com>
2024-07-14 09:53:48 +0700

    add: zig-read-file-line-by-line

---
+++ zig-read-file-line-by-line/src/main.zig
@@ -0,0 +1,37 @@
+const std = @import("std");
+const fs = std.fs;
+const print = std.debug.print;
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    const file = try fs.cwd().openFile("tests/zig-zen.txt", .{});
+    defer file.close();
+
+    var buf_reader = std.io.bufferedReader(file.reader());
+    const reader = buf_reader.reader();
+
+    var line = std.ArrayList(u8).init(allocator);
+    defer line.deinit();
+
+    const writer = line.writer();
+    var line_no: usize = 0;
+    while (reader.streamUntilDelimiter(writer, '\n', null)) {
+        defer line.clearRetainingCapacity();
+        line_no += 1;
+
+        std.debug.print("{d}--{s}\n", .{ line_no, line.items });
+    } else |err| switch (err) {
+        error.EndOfStream => {
+            if (line.items.len > 0) {
+                line_no += 1;
+                print("{d}--{s}\n", .{ line_no, line.items });
+            }
+        },
+        else => return err,
+    }
+
+    print("Total lines: {d}\n", .{line_no});
+}
